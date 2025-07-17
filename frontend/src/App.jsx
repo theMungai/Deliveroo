49823ecada2d8ed3080c66be15f45ae5ee68c3b8
theMungai <mungaithesimon@gmail.com>
2025-07-18 02:15:46 +0300

    Updates " OrderForm"  component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import './App.css';
 import './index.css';
@@ -11,28 +10,17 @@ import Admin_SignUp from './pages/AdminLogin_SignUp.jsx';
 import Profile from './pages/Profile.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import ShippingDetails from './pages/ShippingDetails.jsx';
-=======
-import './App.css';
-import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
->>>>>>> 8979de3 (Creates User dashboard and Admin Panel)
 
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
 
       {/* Optional wrapper div if needed */}
       <div className="bg-gray-100 font-impact min-h-screen">
         <LandingPage />
       </div>
 
-=======
-      <Layout>
-        <Dashboard />
-      </Layout>
->>>>>>> 8979de3 (Creates User dashboard and Admin Panel)
     </>
   );
 }
