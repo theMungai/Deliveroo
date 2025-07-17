113ceec5a18d8f394fda3f5fa518aec1d4d0396d
theMungai <mungaithesimon@gmail.com>
2025-07-18 01:41:17 +0300

    Fix merge conflicts and update User Dashboard and Admin Panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import './App.css';
 import './index.css';
@@ -10,17 +11,28 @@ import Admin_SignUp from './pages/AdminLogin_SignUp.jsx';
 import Profile from './pages/Profile.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import ShippingDetails from './pages/ShippingDetails.jsx';
+=======
+import './App.css';
+import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
+>>>>>>> 8979de3 (Creates User dashboard and Admin Panel)
 
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
 
       {/* Optional wrapper div if needed */}
       <div className="bg-gray-100 font-impact min-h-screen">
         <LandingPage />
       </div>
 
+=======
+      <Layout>
+        <Dashboard />
+      </Layout>
+>>>>>>> 8979de3 (Creates User dashboard and Admin Panel)
     </>
   );
 }
