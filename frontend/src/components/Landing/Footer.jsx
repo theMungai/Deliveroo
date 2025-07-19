34c259aa26f3e966066194e3cb57a434235b006a
theMungai <mungaithesimon@gmail.com>
2025-07-19 13:55:22 +0300

    Fix Heropage and Footer

--- frontend/src/components/Landing/Footer.jsx
+++ frontend/src/components/Landing/Footer.jsx
@@ -1,36 +1,69 @@
-import React from 'react';
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong, faCity, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import cube from "../../assets/cube.png";
 
 const Footer = () => {
+    const addresses = [
+        {label: "123 Jogoo Road, Nairobi", icon : <FontAwesomeIcon icon={faCity} />},
+        {label: "+254 795 482 911", icon : <FontAwesomeIcon icon={faPhone} />},
+        {label: "deliveroo@gmail.com", icon : <FontAwesomeIcon icon={faEnvelope} />},
+    ]
+
+    const address = addresses.map((item) => (
+        <li key={item} className="flex items-center gap-x-3 text-white mt-2">
+            {item.icon}
+            <span className="text-[#FFFFFFC2] text-[14px]">{item.label}</span>
+        </li>
+    ));
+
+    const quickLinks = [
+        {label : "Our Services"},
+        {label : "How it Works"},
+        {label : "Home"},
+    ]
+
+    const quickLink = quickLinks.map((item) => (
+        <li key={item} className="text-[14px] text-[#FFFFFFB8] hover:text-[#73C322B8] hover:underline cursor-pointer mb-2">{item.label}</li>
+    ))
+
   return (
     <>
-    <div className='flex justify-between p-20 bg-black text-white'> 
-        <div>
-            <h1 className='text-2xl font-bold text-green-500'>Deliveroo</h1>
-            <p className=''>123 Jogoo Road, Nairobi</p>
-            <p className=''>+254 795 482 911</p>
-            <p className=''>deliveroo@gmail.com</p>
-        </div>
-        <div className='justify-center'>
-            <p className='font-bold'>Quick Links</p>
-            <p className='font-bold'>Our Services</p>
-            <p className='font-bold'>Our Services</p>
-            <p className='font-bold'>How it Works</p>
-            <p className='font-bold'>Home</p>
-        </div>
-        <div>
-            <p className='font-bold'>Start shipping with us today!</p>
-            <button className='bg-green-500 text-white py-1 px-4 rounded'>Start Now </button>
-        </div>
-        <div>
-            
-        </div>
-    </div>
-    <div className='flex justify-center p-4 bg-black text-white'>
-        <p>© 2023 Deliveroo. All rights reserved.</p>
-    </div>
-    </>
-  )
-}
+      <div className="bg-[#09090B]">
+        <div className="flex justify-between text-white  px-[100px] py-[50px] w-full">
+          <div>
+            <div className="flex items-center gap-x-3.5">
+              <img src={cube} alt="" />
+              <h1 className="text-2xl font-bold text-[#73C322]">Deliveroo</h1>
+            </div>
 
-export default Footer
+            <ul>{address}</ul>
+          </div>
+          <div className="justify-center">
+            <p className="font-[500] text-[20px] mb-2.5 text-[#73C322] ">Quick Links</p>
+            <ul>{quickLink}</ul>
+          </div>
+
+          <div className="relative">
+            <p className="font-bold">
+              Let’s start with us in shipping{" "}
+              <span className="text-[#73C322B8]">your goods</span>
+            </p>
+            <button className="bg-[#73C322] text-white mt-5 py-2 px-5 rounded-[6px] flex items-center gap-x-2.5 absolute right-0 cursor-pointer group">
+              Start Now
+              <FontAwesomeIcon
+                icon={faArrowRightLong}
+                className="transition-transform transform group-hover:translate-x-3.5 ease-in-out duration-500 "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center p-4 text-white">
+          <p>© 2025 Deliveroo. All rights reserved.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
